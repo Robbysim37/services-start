0b0f2a00f37c124a761c28e3edfa1d3297262f6a
Robert Lewis <Robbysim37@gmail.com>
2024-01-17 14:45:02 -0500

    services tutorial done

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,8 +5,7 @@ import { AccountsService } from '../accounts.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService,AccountsService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
 
